5b44e8e6ac0a6e97bc3048b286b46e42562cc88f
magicM00nchild <dorit.wanner@gmx.de>
2026-01-02 10:01:30 +0100

    check preload image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,31 @@
 import sword from "../assets/sword.PNG";
 import "./Home.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [loaded, setLoaded] = useState(false);
+
   return (
     <section className="home">
-      <div className="home-image">
-        <img src={sword} alt="a picture of a sword in a magical light" />
+      <div className="home-image" >
+        {!loaded && (
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              background: "rgba(0,0,0,0.05)",
+              borderRadius: "8px",
+              position: "absolute",
+              top: 0,
+              left: 0,
+            }}
+          />
+        )}
+        <img
+          src={sword}
+          alt="a picture of a sword in a magical light"
+          onLoad={() => setLoaded(true)}
+        />
       </div>
       <div className="home-content">
         <div className="ger">
